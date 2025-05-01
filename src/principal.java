377a9eb2e4cc033d56913659f635999e85858223
OscarEdu2709 <i202501166@cibertec.edu.pe>
2025-04-30 23:53:39 -0500

    MI CAMBIO OSCAR

--- src/principal.java
+++ src/principal.java
@@ -59,6 +59,6 @@ public class principal extends JFrame implements ActionListener {
 		}
 	}
 	protected void actionPerformedBtnNewButton(ActionEvent e) {
-		txtO.append("sii, estoy vivo \n");
+		txtO.append("sii, estoy vivo VENEZUELAAAAAAAA \n");
 	}
 }
