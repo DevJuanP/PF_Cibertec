9ec1a936e6bd1561f21dc15b0b037db3e00aba85
JuanP <juanpajueloch@gmail.com>
2025-04-30 23:49:16 -0500

    second commit

---
+++ src/principal.java
@@ -0,0 +1,64 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class principal extends JFrame implements ActionListener {
+
+	private JPanel contentPane;
+	private JTextArea txtO;
+	private JButton btnNewButton;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					principal frame = new principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public principal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		{
+			txtO = new JTextArea();
+			txtO.setBounds(52, 77, 299, 116);
+			contentPane.add(txtO);
+		}
+		{
+			btnNewButton = new JButton("vivo??");
+			btnNewButton.addActionListener(this);
+			btnNewButton.setBounds(140, 29, 89, 23);
+			contentPane.add(btnNewButton);
+		}
+	}
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnNewButton) {
+			actionPerformedBtnNewButton(e);
+		}
+	}
+	protected void actionPerformedBtnNewButton(ActionEvent e) {
+		txtO.append("sii, estoy vivo \n");
+	}
+}
