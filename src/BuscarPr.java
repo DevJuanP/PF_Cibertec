25cba8c73724f594475f9e857f861add6f912828
JuanP <juanpajueloch@gmail.com>
2025-05-07 01:45:04 -0500

    buscar listo

--- src/BuscarPr.java
+++ src/BuscarPr.java
@@ -1,40 +1,62 @@
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import javax.swing.*;
+
+
 public class BuscarPr extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					BuscarPr frame = new BuscarPr();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
+	
+	public BuscarPr() {
+		setTitle("inicio de sesion");
+		setSize(500, 400);
+		setLocationRelativeTo(null);
+		setLayout(null);
+		
+		JLabel lblBuscar = new JLabel("Nombre a buscar: ");
+		lblBuscar.setBounds(30, 30, 120, 25);
+		
+		JTextField  txtBuscar = new JTextField();
+		txtBuscar.setBounds(160, 30, 120, 25);
+		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.setBounds(150, 80, 150, 25);
+		
+		JTextArea txtResultados = new JTextArea();
+		
+		JScrollPane sp = new JScrollPane();
+		sp.setViewportView(txtResultados);
+		sp.setBounds(80, 140, 330, 170);
+		
+		add(lblBuscar);
+		add(txtBuscar);
+		add(btnBuscar);
+		add(sp);
+		
+		//lógica:
+		btnBuscar.addActionListener(e -> {
+			String txtToAsk = txtBuscar.getText().toLowerCase();
+			txtResultados.setText("");
+			for(ConstructorPR p:RegistrarPr.inventario) {
+				if(p.nombre.toLowerCase().contains(txtToAsk)) {
+					txtResultados.append("id: " + p.ID + "\n"+
+										"Producto: " + p.nombre + "\n"+
+										"Cantidad : " + p.cantidad + "\n"+
+										"Precio: " + p.precio + "\n\n");
 				}
 			}
+			
+			if (txtResultados.getText().isEmpty()) txtResultados.setText("Producto no encontrado");
 		});
+		
+
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public BuscarPr() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+	
+	public static void main(String[] args) {
+		new BuscarPr().setVisible(true);
 	}
-
 }
--- src/RegistrarPr.java
+++ src/RegistrarPr.java
@@ -41,7 +41,6 @@ public class RegistrarPr extends JFrame {
 		
 		setFont(new Font("Arial", Font.PLAIN, 13));
 		setTitle("AGREGAR PRODUCTO");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
