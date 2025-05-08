9da1ae86ed684e0d1003e914254aa0314f577588
JuanP <juanpajueloch@gmail.com>
2025-05-07 23:35:36 -0500

    modificar al 80%

--- src/ModificarPR.java
+++ src/ModificarPR.java
@@ -1,44 +1,80 @@
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import javax.swing.*;
 
 public class ModificarPR extends JFrame {
-
+	private ConstructorPR PrAmodificar = null;
 
 	public ModificarPR() {
 		setTitle("modificar producto");
-		setSize(380, 380);
+		setSize(338, 447);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(null);
 		
 		//componenetes
 		JLabel lblAsk = new JLabel("Escriba el id: ");
-		lblAsk.setBounds(50, 30, 100, 25);
+		lblAsk.setBounds(29, 11, 100, 25);
 		
 		JTextField txtIdToFinf = new JTextField();
-		txtIdToFinf.setBounds(182, 30, 100, 25);
-		
+		txtIdToFinf.setBounds(128, 11, 100, 25);
+		//
 		JButton btnBuscar = new JButton("Buscar");
-		btnBuscar.setBounds(180, 70, 100, 25);
-		
+		btnBuscar.setBounds(180, 51, 100, 25);
+		//
 		JTextArea txtInfo = new JTextArea();
-		txtInfo.setBounds(50, 100, 230, 80);
+		txtInfo.setBounds(50, 87, 230, 118);
+		//
+		JLabel txtNewInfo = new JLabel("Coloque a continuación los nuevos valores");
+		txtNewInfo.setBounds(29, 212, 240, 25);
+		//nuevo nombre:
+		JLabel lblNewNombre = new JLabel("Nuevo nombre: ");
+		lblNewNombre.setBounds(49, 248, 100, 25);
+		
+		JTextField txtNewName = new JTextField();
+		txtNewName.setBounds(145, 248, 139, 25);
+		//nueva  cantidad:
+		JLabel lblNewCantidad = new JLabel("Nueva Cantidad: ");
+		lblNewCantidad.setBounds(49, 284, 100, 25);
+		
+		JTextField txtNewCantidad = new JTextField();
+		txtNewCantidad.setBounds(145, 284, 139, 25);
+		//nuevo precio
+		JLabel lblNewPrecio = new JLabel("Nuevo Precio:");
+		lblNewPrecio.setBounds(49, 320, 100, 25);
+		
+		JTextField txtNewPrecio = new JTextField();
+		txtNewPrecio.setBounds(145, 320, 139, 25);
+		//boton modificar
+		JButton btnModificar = new JButton("MODIFICAR");
+		btnModificar.setBounds(99, 372, 100, 25);
+		
 		
 		//add:
 		getContentPane().add(lblAsk);
 		getContentPane().add(txtIdToFinf);
 		getContentPane().add(btnBuscar);
 		getContentPane().add(txtInfo);
+		getContentPane().add(txtNewInfo);
+		
+		getContentPane().add(lblNewNombre);
+		getContentPane().add(txtNewName);
+		
+		getContentPane().add(lblNewCantidad);
+		getContentPane().add(txtNewCantidad);
+		
+		getContentPane().add(lblNewPrecio);
+		getContentPane().add(txtNewPrecio);
+		
+		getContentPane().add(btnModificar);
 		
 		//logica
+		txtInfo.setEditable(false);
+		
 		btnBuscar.addActionListener(e -> {
 			int idBuscado = Integer.parseInt(txtIdToFinf.getText());
 			System.out.println("id: "+idBuscado);
-			ConstructorPR PrAmodificar = null;
 			
 			for(ConstructorPR p:RegistrarPr.inventario) {
 				if(p.ID == idBuscado) {
@@ -48,13 +84,32 @@ public class ModificarPR extends JFrame {
 			}
 			if(PrAmodificar == null) {
 				txtInfo.setText("Producto no encontrado");
+				txtNewName.setText("");
+				txtNewCantidad.setText("");
+				txtNewPrecio.setText("");
 			}else {
-				txtInfo.setText("id: " + PrAmodificar.ID + "\n"+
+				txtInfo.setText("Producto a modificar: \n\n");
+				txtInfo.append("id: " + PrAmodificar.ID + "\n"+
 						"Producto: " + PrAmodificar.nombre + "\n"+
 						"Cantidad : " + PrAmodificar.cantidad + "\n"+
 						"Precio: " + PrAmodificar.precio);
+				
+				txtNewName.setText(PrAmodificar.nombre);
+				txtNewCantidad.setText(""+PrAmodificar.cantidad);
+				txtNewPrecio.setText(""+PrAmodificar.precio);
+				
+				txtIdToFinf.setText("");
+				
 			}
 		});
+		
+		btnModificar.addActionListener(e -> {
+			if (PrAmodificar == null) {
+				JOptionPane.showMessageDialog(null, "No se ha seleccionado ningún producto a modificar");
+				return;
+			}
+		});
+		
 	}
 
 	public static void main(String[] args) {
